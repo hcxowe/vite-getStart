c9d6efd436af688a15d80ac86066cbc6417a0509
hcxowe <hcxowe@126.com>
2022-04-27 17:09:31 +0800

    打包后兼容传统浏览器

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vue from '@vitejs/plugin-vue'         // 支持vue
+import vueJsx from '@vitejs/plugin-vue-jsx'  // 支持jsx，tsx
+import legacy from '@vitejs/plugin-legacy'   // 支持传统浏览器   
+
 import path from 'path'
 import postcssNesting from 'postcss-nesting'
 import autoPreFixer from 'autoprefixer'
 
 export default defineConfig({
-    plugins: [vue(), vueJsx()],
+    plugins: [
+        vue(), 
+        vueJsx(), 
+        legacy({
+            targets: ['defaults', 'not IE 11']
+        })
+    ],
     css: {
         modules: {
             localsConvention: 'camelCase', // 启用样式名称转换驼峰，可以按名称导入
