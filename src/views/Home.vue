22c1fca87af388b08e5174ecf7b5fca8e8be553d
hcxowe <hcxowe@126.com>
2022-05-07 09:39:35 +0800

    更新title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="title">Vue3 + TypeScript + Vite + Pinia</h1>
+    <h1 class="title">Vue3 + TypeScript + Vite + Pinia2 + Vue-Router4 + lodash</h1>
     <p>{{ '账号：' + userStore.username + ' 密码: ' + userStore.password }}</p>
     <p class="version-title">项目版本： {{ env.VITE_APP_TITLE + env.VITE_APP_VERSION }}</p>
 </template>
