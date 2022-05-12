5ad53ff33adb7c728a4dd2d8af38ef7436613377
hcxowe <hcxowe@126.com>
2022-05-12 22:59:03 +0800

    eslint: 更新配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
         'no-eval': 'error',                         // 禁用 eval()
         'no-implied-eval': 'error',                 // 禁止使用类似 eval() 的方法
         'no-labels': 'error',                       // 禁用标签语句
-        'no-magic-numbers': 'error',                // 禁用魔术数字
+        //'no-magic-numbers': 'error',                // 禁用魔术数字
         'no-unmodified-loop-condition': 'error',	// 禁用一成不变的循环条件，防止死循环
         'no-useless-return': 'error',               // 禁止多余的 return 语句 ☻
         'require-await': 'error',					// 禁止不带 await 的 async
@@ -65,7 +65,7 @@ module.exports = {
         ],   
         
         /* Stylistic Issues */
-        'func-call-spacing': ['error', 'never'],    // 禁止在函数标识符和其调用之间有空格 如：fun ()
+        //'func-call-spacing': ['error', 'never'],    // 禁止在函数标识符和其调用之间有空格 如：fun ()
         'key-spacing': 'error',						// 强制对象属性 : 后需要一个空格
         'indent': [
             'error',
@@ -93,8 +93,11 @@ module.exports = {
 		'comma-style': 'error',						// 逗号放在数组元素，对象属性值之后，且在同一行
 		'computed-property-spacing': 'error',		// obj[ 'a' ] 不允许出现空格
 		'new-cap': 'error',							// 构造函数首字母大写
-		'no-multiple-empty-lines': 'error',			// 限制最多出现两个空行
-		'no-trailing-spaces': 'error', 				// 禁止在空行使用空白字符
+		'no-multiple-empty-lines': [                // 限制最多出现两个空行
+            "error", 
+            { "max": 2 }
+        ],			
+		'no-trailing-spaces': 'error', 				// 禁用行尾空格☻
     	'no-unneeded-ternary': 'error', 			// 禁止多余的三元表达式，如a === 1 ? true : false应缩写为 a === 1
     	'no-whitespace-before-property': 'error', 	// 禁止属性前有空白，如console. log(obj['a'])，log前面的空白有问题
     	'nonblock-statement-body-position': 'error',// 强制单行语句不换行
@@ -145,16 +148,24 @@ module.exports = {
                 'exceptAfterSingleLine': true 
             }
         ],
+        'space-unary-ops': "error",                 // 单词类一元操作符后需要添加空格，非单词类不能添加空格
+        'switch-colon-spacing': 'error',            // 在 switch 的冒号后添加一个或多个空格
 		
 		/* ES6 */
-    	//'arrow-body-style': 'error', 				// 当箭头函数体的花括号可以省略时，不允许出现花括号
-    	// 'arrow-parens': [							// 箭头函数参数只有一个时，不允许写圆括号
-		// 	'error', 
-		// 	'as-needed'
-		// ], 	
+    	'arrow-body-style': [                       // 当箭头函数体的花括号可以省略时，不允许出现花括号
+            "error", 
+            "as-needed",
+            { "requireReturnForObjectLiteral": true } // 显示 return 对象字面量
+        ], 				
+    	'arrow-parens': [							// 箭头函数参数只有一个时，不允许写圆括号
+			'error', 
+			'as-needed',
+            { "requireForBlockBody": true }         // 如果函数体在一个指令块中（被花括号括起来）要求使用圆括号把参数括起来
+		], 	
     	'arrow-spacing': 'error', 					// 要求箭头函数的 => 前后有空格
+        'constructor-super': "error",                 // 派生类中构造函数强制调用 super()
     	'no-confusing-arrow': 'error', 				// 禁止在可能与比较操作符混淆的地方使用箭头函数
-    	//'no-duplicate-imports': 'error', 			// 禁止重复导入
+    	'no-duplicate-imports': 'error', 			// 禁止重复导入
     	'no-var': 'error', 							// 要求使用let或const，而不是var
     	'object-shorthand': 'error', 				// 要求对象字面量使用简写
     	'prefer-const': 'error', 					// 要求使用const声明不会被修改的变量
