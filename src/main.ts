50a778b9749435bbc2d7ca477ab5f9598458661b
hcxowe <hcxowe@126.com>
2022-05-22 23:01:01 +0800

    修改为sass样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router/index'
 import i18n from './plugins/i18n'
-import './styles/index.css'
+import './styles/index.scss'
 
 createApp(App).use(createPinia()).use(router).use(i18n).mount('#app')
